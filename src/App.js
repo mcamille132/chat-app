6d94ac965564440f6676aae3d3d5057a63be7718
Marie-Camille <mariecamille.dorio@gmail.com>
2021-04-17 21:57:05 +0200

    React02.2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Contact from './components/Contact'
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact avatar="https://randomuser.me/api/portraits/men/1.jpg"
+                name ="Tomothy Lawson"
+                status />
+      <Contact avatar="https://randomuser.me/api/portraits/men/20.jpg"
+                name ="Ben Turner"
+                 />
+      <Contact avatar="https://randomuser.me/api/portraits/women/89.jpg"
+                name ="Beverly Campbell"
+                status />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
-function Contact () {
-    const avatar = "https://randomuser.me/api/portraits/men/1.jpg";
-    const name = "Tomothy Lawson"
-    const status = true;
+function Contact (props) {
+    //const avatar = "https://randomuser.me/api/portraits/men/1.jpg";
+    //const name = "Tomothy Lawson"
+    //const status = true;
     return (
         <div className="Contact"> 
-            <img src={avatar} className="avatar" alt="avatar"/>
-            
-            <div className="">
-            <p className="status name status-text">{name}</p>
-            <p className="status"><span className="status-online"></span>{status ? "Online" : "Offline"}</p>
-            </div>
-        
-            
+            <img src={props.avatar} className="avatar" alt="avatar"/>
+                <div>
+                    <p className="status name status-text">{props.name}</p>
+                    <p className="status"><span className={props.status ? 'status-online' : 'status-offline'}></span>{props.status ? "Online" : "Offline"}</p>
+                </div>
         </div>
-    )
+    );
 }
+Contact.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    status: PropTypes.bool.isRequired,
+  };
 
 export default Contact;
