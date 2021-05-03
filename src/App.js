3341bff6d45dd34f27eaea215cf9976335c7161a
Marie-Camille <mariecamille.dorio@gmail.com>
2021-05-03 11:59:47 +0200

    Add ContactList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-
-import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact avatar="https://randomuser.me/api/portraits/men/1.jpg"
-                name ="Tomothy Lawson"
-                status />
-      <Contact avatar="https://randomuser.me/api/portraits/men/20.jpg"
-                name ="Ben Turner"
-                 />
-      <Contact avatar="https://randomuser.me/api/portraits/women/89.jpg"
-                name ="Beverly Campbell"
-                status />
+      <ContactList />
     </div>
   );
 }
